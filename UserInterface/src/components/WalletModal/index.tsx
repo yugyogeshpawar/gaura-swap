24ca67f7bec5fda5b77bd1aa59f90e017fe3b1e2
Mystical94 <shubhamgupta6428@gmail.com>
2022-08-13 18:01:32 +0530

    CloudScan to GauraScan

--- UserInterface/src/components/WalletModal/index.tsx
+++ UserInterface/src/components/WalletModal/index.tsx
@@ -289,7 +289,7 @@ export default function WalletModal({
           <HeaderRow>{error instanceof UnsupportedChainIdError ? 'Wrong Network' : 'Error connecting'}</HeaderRow>
           <ContentWrapper>
             {error instanceof UnsupportedChainIdError ? (
-              <h5>Please connect to the appropriate Cloud network.</h5>
+              <h5>Please connect to the appropriate Gaura network.</h5>
             ) : (
               'Error connecting. Try refreshing the page.'
             )}
